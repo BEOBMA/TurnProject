720f0e42b7a98f55a9d2fe7b517ef2b27b4e58c9
BEOBMA <dingxiao19@gmail.com>
2024-08-15 22:53:37 +0900

    refactor: 사전 용어 추가

--- src/main/java/org/beobma/projectturngame/localization/Dictionary.kt
+++ src/main/java/org/beobma/projectturngame/localization/Dictionary.kt
@@ -7,17 +7,21 @@ import org.beobma.projectturngame.text.TextColorType
 class Dictionary {
     val dictionaryList: HashMap<String, Component> = hashMapOf(
         Pair(
-            "마나", KeywordType.Mana.component.append(Component.text(": 대부분의 카드를 사용할 때 소모되는 자원입니다.", TextColorType.Gray.textColor))
+            "마나", KeywordType.Mana.component.append(Component.text(": 카드를 사용할 떄 소모된다.", TextColorType.Gray.textColor))
         ), Pair(
-            "잔존", KeywordType.Remnant.component.append(Component.text(": 이 효과가 붙은 카드 사용 시 패로 되돌아옵니다.", TextColorType.Gray.textColor))
+            "잔존", KeywordType.Remnant.component.append(Component.text(": 사용 시 묘지로 보내지지 않고 패로 되돌아온다.", TextColorType.Gray.textColor))
         ), Pair(
-            "제외", KeywordType.Banish.component.append(Component.text(": 일부 효과로 인해 카드가 제외될 수 있습니다. 제외된 카드는 이번 전투동안 사용할 수 없습니다.", TextColorType.Gray.textColor))
+            "제외", KeywordType.Banish.component.append(Component.text(": 제외된 카드는 이번 전투동안 사용할 수 없다.", TextColorType.Gray.textColor))
         ), Pair(
-            "묘지", KeywordType.Graveyard.component.append(Component.text(": 카드를 사용하거나 버리면 묘지로 보내집니다. 카드를 뽑을 때, 덱에 남은 카드가 없다면 묘지에 있는 카드를 덱으로 되돌립니다.", TextColorType.Gray.textColor))
+            "묘지", KeywordType.Graveyard.component.append(Component.text(": 카드를 사용하거나 버리면 묘지로 보내진다. 덱에서 카드를 뽑을 때, 덱에 남은 카드가 없다면 묘지에 있는 카드를 덱으로 되돌린다.", TextColorType.Gray.textColor))
         ), Pair(
-            "소멸", KeywordType.Extinction.component.append(Component.text(": 이 효과가 붙은 카드 사용 시 이 카드가 제외됩니다.", TextColorType.Gray.textColor))
+            "소멸", KeywordType.Extinction.component.append(Component.text(": 사용 시 묘지로 보내지지 않고, 제외된다.", TextColorType.Gray.textColor))
         ), Pair(
-            "동일 카드 소멸", KeywordType.SameCardDisappears.component.append(Component.text(": 이 효과가 붙은 카드 사용 시 덱, 묘지에 있는 동일한 카드를 모두 제외합니다.", TextColorType.Gray.textColor))
+            "동일 카드 소멸", KeywordType.SameCardDisappears.component.append(Component.text(": 사용 시 덱, 묘지에 있는 동일한 카드를 모두 제외한다.", TextColorType.Gray.textColor))
+        ), Pair(
+            "흐림", KeywordType.Cloudy.component.append(Component.text(": 특정 상태이상과 시너지를 발동하고 소멸한다.", TextColorType.Gray.textColor))
+        ), Pair(
+            "전격", KeywordType.Electroshock.component.append(Component.text(": ", TextColorType.Gray.textColor).append(KeywordType.Cloudy.component.append(Component.text(" 상태이상과 동시에 존재한다면, 두 상태이상을 제거하고 18의 고정 피해를 입는다.", TextColorType.Gray.textColor))) )
         )
     )
 }
