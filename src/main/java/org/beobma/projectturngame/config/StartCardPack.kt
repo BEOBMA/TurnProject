7e299791a66ade9243af267fc8f75205a0523997
BEOBMA <dingxiao19@gmail.com>
2024-08-17 13:21:41 +0900

    refactor: 코드 폴딩 주석 / 람다 위치 변경

--- src/main/java/org/beobma/projectturngame/config/StartCardPack.kt
+++ src/main/java/org/beobma/projectturngame/config/StartCardPack.kt
@@ -25,96 +25,111 @@ class StartCardPack {
     }
 
     private fun cardConfig() {
+        //region attack Initialization
         val attack = Card(
             "공격", listOf(
                 Component.text("바라보는 적에게 5의 피해를 입힌다.", TextColorType.Gray.textColor)
-            ), CardRarity.Common, 1
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val player = usePlayerData.player
-                val target = usePlayerData.focusOn()
+            ), CardRarity.Common, 1,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val player = usePlayerData.player
+                    val target = usePlayerData.focusOn()
 
-                if (target !is Enemy) {
-                    val targetingFailText = textManager.targetingFailText()
-                    player.sendMessage(targetingFailText)
-                    soundManager.run { player.playTargetingFailSound() }
-                    return@Card false
-                }
+                    if (target !is Enemy) {
+                        val targetingFailText = textManager.targetingFailText()
+                        player.sendMessage(targetingFailText)
+                        soundManager.run { player.playTargetingFailSound() }
+                        return@Card false
+                    }
 
-                enemyManager.run {
-                    target.damage(5, usePlayerData)
+                    enemyManager.run {
+                        target.damage(5, usePlayerData)
+                    }
                 }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region heal Initialization
         val heal = Card(
             "가벼운 치유", listOf(
                 Component.text("체력을 5 회복한다.", TextColorType.Gray.textColor)
-            ), CardRarity.Common, 1
-        ) { usePlayerData ->
-            playerManager.run {
-                usePlayerData.heal(5, usePlayerData)
+            ), CardRarity.Common, 1,
+            { usePlayerData ->
+                playerManager.run {
+                    usePlayerData.heal(5, usePlayerData)
+                }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region powerAttack Initialization
         val powerAttack = Card(
             "강공", listOf(
                 Component.text("바라보는 적에게 12의 피해를 입힌다", TextColorType.Gray.textColor)
-            ), CardRarity.Common, 2
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val player = usePlayerData.player
-                val target = usePlayerData.focusOn()
+            ), CardRarity.Common, 2,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val player = usePlayerData.player
+                    val target = usePlayerData.focusOn()
 
-                if (target !is Enemy) {
-                    val targetingFailText = textManager.targetingFailText()
-                    player.sendMessage(targetingFailText)
-                    soundManager.run { player.playTargetingFailSound() }
-                    return@Card false
-                }
+                    if (target !is Enemy) {
+                        val targetingFailText = textManager.targetingFailText()
+                        player.sendMessage(targetingFailText)
+                        soundManager.run { player.playTargetingFailSound() }
+                        return@Card false
+                    }
 
-                enemyManager.run {
-                    target.damage(12, usePlayerData)
+                    enemyManager.run {
+                        target.damage(12, usePlayerData)
+                    }
                 }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region rest Initialization
         val rest = Card(
             "휴식", listOf(
                 KeywordType.Mana.component.append(Component.text("를 1 회복한다.", TextColorType.Gray.textColor))
-            ), CardRarity.Common, 0
-        ) { usePlayerData ->
-            playerManager.run {
-                usePlayerData.addMana(1)
+            ), CardRarity.Common, 0,
+            { usePlayerData ->
+                playerManager.run {
+                    usePlayerData.addMana(1)
+                }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region continuousAttack Initialization
         val continuousAttack = Card(
             "연공", listOf(
                 Component.text("바라보는 적에게 20의 피해를 입힌다.", TextColorType.Gray.textColor)
-            ), CardRarity.Common, 3
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val player = usePlayerData.player
-                val target = usePlayerData.focusOn()
+            ), CardRarity.Common, 3,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val player = usePlayerData.player
+                    val target = usePlayerData.focusOn()
 
-                if (target !is Enemy) {
-                    val targetingFailText = textManager.targetingFailText()
-                    player.sendMessage(targetingFailText)
-                    soundManager.run { player.playTargetingFailSound() }
-                    return@Card false
-                }
+                    if (target !is Enemy) {
+                        val targetingFailText = textManager.targetingFailText()
+                        player.sendMessage(targetingFailText)
+                        soundManager.run { player.playTargetingFailSound() }
+                        return@Card false
+                    }
 
-                enemyManager.run {
-                    target.damage(20, usePlayerData)
+                    enemyManager.run {
+                        target.damage(20, usePlayerData)
+                    }
                 }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
 
         startCardList.addAll(
