8e4fe457a6e26a095980eca59d26237b3d690803
BEOBMA <dingxiao19@gmail.com>
2024-08-17 13:21:50 +0900

    refactor: 코드 폴딩 주석 / 람다 위치 변경

--- src/main/java/org/beobma/projectturngame/config/HarmonyOfNatureCardPack.kt
+++ src/main/java/org/beobma/projectturngame/config/HarmonyOfNatureCardPack.kt
@@ -34,156 +34,166 @@ class HarmonyOfNatureCardPack {
             ), mutableListOf()
         )
 
+        //region sea Initialization
         val sea = Card(
             "바다", listOf(
                 Component.text("모든 아군의 체력을 5 회복시킨다.", TextColorType.Gray.textColor),
                 Component.text("이번 턴에 사용하는 '식물' 카드의 위력이 3 증가한다.", TextColorType.Gray.textColor),
                 Component.text("위 효과는 중첩되지 않는다.", TextColorType.DarkGray.textColor)
-            ), CardRarity.Uncommon, 1
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val target = usePlayerData.allTeamMembers(excludeSelf = true, includeDeceased = false)
+            ), CardRarity.Uncommon, 1,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val target = usePlayerData.allTeamMembers(excludeSelf = true, includeDeceased = false)
 
-                playerManager.run {
-                    target.forEach {
-                        it.heal(5, usePlayerData)
+                    playerManager.run {
+                        target.forEach {
+                            it.heal(5, usePlayerData)
+                        }
+                        usePlayerData.addTag("seaTag", ResetType.TurnEnd)
                     }
-                    usePlayerData.addTag("seaTag", ResetType.TurnEnd)
                 }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region plants Initialization
         val plants = Card(
             "식물", listOf(
                 Component.text("바라보는 적에게 6의 피해를 입힌다.", TextColorType.Gray.textColor)
-            ), CardRarity.Common, 1
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val player = usePlayerData.player
-                val target = usePlayerData.focusOn()
+            ), CardRarity.Common, 1,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val player = usePlayerData.player
+                    val target = usePlayerData.focusOn()
 
-                if (target !is Enemy) {
-                    val targetingFailText = textManager.targetingFailText()
-                    player.sendMessage(targetingFailText)
-                    soundManager.run { player.playTargetingFailSound() }
-                    return@Card false
-                }
+                    if (target !is Enemy) {
+                        val targetingFailText = textManager.targetingFailText()
+                        player.sendMessage(targetingFailText)
+                        soundManager.run { player.playTargetingFailSound() }
+                        return@Card false
+                    }
 
-                if (player.scoreboardTags.contains("seaTag")) {
-                    enemyManager.run {
-                        target.damage(9, usePlayerData)
+                    if (player.scoreboardTags.contains("seaTag")) {
+                        enemyManager.run {
+                            target.damage(9, usePlayerData)
+                        }
+                    }
+                    else {
+                        enemyManager.run {
+                            target.damage(6, usePlayerData)
+                        }
                     }
                 }
-                else {
-                    enemyManager.run {
-                        target.damage(6, usePlayerData)
-                    }
-                }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region sky Initialization
         val sky = Card(
             "하늘", listOf(
                 Component.text("카드를 2장 뽑는다.", TextColorType.Gray.textColor),
                 Component.text("이번 턴에 카드의 효과로 '비구름'을 뽑을 때, '낙뢰' 카드를 추가로 1장 생성하고 뽑는다.", TextColorType.Gray.textColor),
                 Component.text("위 효과는 중첩되지 않는다.", TextColorType.DarkGray.textColor),
-                Component.text("[ 낙뢰 | (1) ]: 모든 적에게 ", TextColorType.DarkGray.textColor).append(KeywordType.Electroshock.component).append(
-                    Component.text("를 적용한다.", TextColorType.Gray.textColor)
-                ),
+                Component.text("[ 낙뢰 | (1) | 휘발 ]: 모든 적에게 전격을 적용한다.", TextColorType.DarkGray.textColor),
+                dictionary.dictionaryList["휘발"]!!,
                 dictionary.dictionaryList["전격"]!!
-            ), CardRarity.Uncommon, 1
-        ) { usePlayerData ->
-            cardManager.run {
-                usePlayerData.drow(2)
-            }
+            ), CardRarity.Uncommon, 1,
+            { usePlayerData ->
+                cardManager.run {
+                    usePlayerData.drow(2)
+                }
 
-            playerManager.run {
-                usePlayerData.addTag("skyTag", ResetType.TurnEnd)
+                playerManager.run {
+                    usePlayerData.addTag("skyTag", ResetType.TurnEnd)
+                }
+                return@Card true
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region rainCloud Initialization
         val rainCloud = Card(
             "비구름", listOf(
+                KeywordType.Volatilization.component,
                 Component.text("모든 적에게 ", TextColorType.Gray.textColor).append(KeywordType.Cloudy.component).append(Component.text("을 적용한다.", TextColorType.Gray.textColor)),
+                dictionary.dictionaryList["휘발"]!!,
                 dictionary.dictionaryList["흐림"]!!
-            ), CardRarity.Common, 0
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val abnormalStatusManager = AbnormalStatusManager()
-                val specialAbnormalStatusManager = abnormalStatusManager.createSpecialAbnormalStatusManager()
-                val target = usePlayerData.allEnemyMembers()
+            ), CardRarity.Common, 0,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val abnormalStatusManager = AbnormalStatusManager()
+                    val specialAbnormalStatusManager = abnormalStatusManager.createSpecialAbnormalStatusManager()
+                    val target = usePlayerData.allEnemyMembers()
 
-                specialAbnormalStatusManager.run {
-                    target.forEach {
-                        it.addCloudy()
+                    specialAbnormalStatusManager.run {
+                        target.forEach {
+                            it.addCloudy()
+                        }
                     }
                 }
+                return@Card true
+            },
+            { usePlayerData ->
+                cardManager.run {
+                    val card = usePlayerData.graveyard.find { it.name == "비구름" }
+                    usePlayerData.graveyard.remove(card)
+                }
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region lightningStrike Initialization
         val lightningStrike = Card(
             "낙뢰", listOf(
+                KeywordType.Volatilization.component,
                 Component.text("모든 적에게 ", TextColorType.Gray.textColor).append(KeywordType.Electroshock.component).append(Component.text("를 적용한다.", TextColorType.Gray.textColor)),
+                dictionary.dictionaryList["휘발"]!!,
                 dictionary.dictionaryList["전격"]!!
-            ), CardRarity.Common, 1
-        ) { usePlayerData ->
-            selectionFactordManager.run {
-                val abnormalStatusManager = AbnormalStatusManager()
-                val specialAbnormalStatusManager = abnormalStatusManager.createSpecialAbnormalStatusManager()
-                val target = usePlayerData.allEnemyMembers()
+            ), CardRarity.Common, 1,
+            { usePlayerData ->
+                selectionFactordManager.run {
+                    val abnormalStatusManager = AbnormalStatusManager()
+                    val specialAbnormalStatusManager = abnormalStatusManager.createSpecialAbnormalStatusManager()
+                    val target = usePlayerData.allEnemyMembers()
 
-                specialAbnormalStatusManager.run {
-                    target.forEach {
-                        it.addElectroshock()
+                    specialAbnormalStatusManager.run {
+                        target.forEach {
+                            it.addElectroshock()
+                        }
                     }
                 }
+                return@Card true
+            },
+            { usePlayerData ->
+                cardManager.run {
+                    val card = usePlayerData.graveyard.find { it.name == "낙뢰" }
+                    usePlayerData.graveyard.remove(card)
+                }
             }
-            return@Card true
-        }
+        )
+        //endregion
 
+        //region cloud Initialization
         val cloud = Card(
             "구름", listOf(
                 Component.text("'비구름' 카드 1장을 생성하고, 패에 넣는다.", TextColorType.Gray.textColor),
-                Component.text("[ 비구름 | (0) ]: 모든 적에게 ", TextColorType.DarkGray.textColor).append(KeywordType.Cloudy.component).append(
-                    Component.text("을 적용한다.", TextColorType.Gray.textColor)
-                ),
+                Component.text("[ 비구름 | (0) | 휘발 ]: 모든 적에게 흐림을 적용한다.", TextColorType.DarkGray.textColor),
+                dictionary.dictionaryList["휘발"]!!,
                 dictionary.dictionaryList["흐림"]!!
-            ), CardRarity.Common, 1
-        ) { usePlayerData ->
-            cardManager.run {
-                usePlayerData.getCard(rainCloud)
-                if (usePlayerData.player.scoreboardTags.contains("skyTag")) {
-                    usePlayerData.getCard(lightningStrike)
+            ), CardRarity.Common, 1,
+            { usePlayerData ->
+                cardManager.run {
+                    usePlayerData.getCard(rainCloud)
+                    if (usePlayerData.player.scoreboardTags.contains("skyTag")) {
+                        usePlayerData.getCard(lightningStrike)
+                    }
                 }
+                return@Card true
             }
-            return@Card true
-        }
-
-
-        val ground = Card(
-            "땅", listOf(
-                Component.text("카드를 2장 뽑는다.", TextColorType.Gray.textColor),
-                Component.text("이번 턴에 카드의 효과로 '비구름'을 뽑을 때, '낙뢰' 카드를 추가로 1장 생성하고 뽑는다.", TextColorType.Gray.textColor),
-                Component.text("위 효과는 중첩되지 않는다.", TextColorType.DarkGray.textColor),
-                Component.text("[ 낙뢰 | (1) ]: 모든 적에게 ", TextColorType.DarkGray.textColor).append(KeywordType.Electroshock.component).append(
-                    Component.text("를 적용한다.", TextColorType.Gray.textColor)
-                ),
-                dictionary.dictionaryList["전격"]!!
-            ), CardRarity.Uncommon, 1
-        ) { usePlayerData ->
-            cardManager.run {
-                usePlayerData.drow(2)
-            }
-
-            playerManager.run {
-                usePlayerData.addTag("skyTag", ResetType.TurnEnd)
-            }
-            return@Card true
-        }
+        )
+        //endregion
 
         cardPack.cardList.addAll(
             listOf(
