0774e39f8267e2ca7693de5eda5d23dd39eadd8f
BEOBMA <ssdssdtkfk@naver.com>
2024-10-12 22:32:08 +0900

    feat: 피격 시 효과를 발동하는 유물

--- src/main/java/org/beobma/projectturngame/listener/OnDamageEvent.kt
+++ src/main/java/org/beobma/projectturngame/listener/OnDamageEvent.kt
@@ -2,8 +2,10 @@ package org.beobma.projectturngame.listener
 
 import org.beobma.projectturngame.abnormalityStatus.AbnormalityStatus
 import org.beobma.projectturngame.entity.Entity
+import org.beobma.projectturngame.entity.player.Player
 import org.beobma.projectturngame.event.EntityDamageEvent
 import org.beobma.projectturngame.text.KeywordType
+import org.beobma.projectturngame.util.EffectTime
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import kotlin.random.Random
@@ -20,6 +22,20 @@ class OnDamageEvent : Listener {
         if (attacker is Entity) {
             weaknessHandler(attacker, event)
             blindnessHandler(attacker, event)
+
+            if (entity is Player) {
+                val entityRelicsList = entity.relics.filter { it.effectTime == EffectTime.WhenHit }
+
+                if (entityRelicsList.isNotEmpty()) {
+                    entityRelicsList.forEach {
+                        val finalDamage = it.effect?.invoke(entity, listOf(attacker, damage, damageType))
+
+                        if (finalDamage is Int) {
+                            event.damage += finalDamage
+                        }
+                    }
+                }
+            }
         }
     }
 
